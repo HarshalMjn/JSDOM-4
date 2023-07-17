aae992fa92807334eb35c5a92d32a466e117fcb5
HarhslaMjn <mahajanh006@gmail.com>
2023-07-17 20:22:38 +0530

    async awiat

--- index.js
+++ index.js
@@ -114,13 +114,38 @@
 
 // }
 
-async function utility() {
-    let content =await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    let output = await content.json();
-    console.log(output);
+// async function utility() {
+//     let content =await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     let output = await content.json();
+//     console.log(output);
 
+// }
+// utility();
+
+async function helper() {
+    let options = { 
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'Harshal',
+          body: 'stronge',
+          userId: 2002,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+    }
+    let Content = await fetch('https://jsonplaceholder.typicode.com/posts',options);
+    let respose = Content.json();
+    return respose;
 }
+
+async function utility() {
+    let ans =await helper();
+    console.log(ans);
+}
+
 utility();
+
 
 
    
